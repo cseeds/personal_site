4c111baf82291fc4efbb604a20cbac22b80c7c20
Chris Seeds <cseeds@gmail.com>
2013-02-26 22:40:55 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,188 @@
+<!DOCTYPE HTML>
+<!-- 
+88        88  88     888888888888  88                                                    
+88        88  ""          88       88                                                    
+88        88              88       88                                                    
+88aaaaaaaa88  88          88       88,dPPYba,    ,adPPYba,  8b,dPPYba,   ,adPPYba,       
+88""""""""88  88          88       88P'    "8a  a8P_____88  88P'   "Y8  a8P_____88       
+88        88  88          88       88       88  8PP"""""""  88          8PP"""""""       
+88        88  88          88       88       88  "8b,   ,aa  88          "8b,   ,aa  888  
+88        88  88          88       88       88   `"Ybbd8"'  88           `"Ybbd8"'  888
+
+
+thanks for looking under the hood.	
+
+	 -->
+
+
+ <head> 
+ <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+ 
+ 
+ <title> Christopher Seeds | Designer </title> 
+ <!-- <link rel="shortcut icon" href="favicon.png">-->
+ 
+ 	<link rel="stylesheet" type="text/css" href="css/temp.css">
+	<!-- Make minor type adjustments for 1024 monitors -->
+	<link rel="stylesheet" href="css/tempM.css" media="only screen and (max-width: 1024px)" />
+	<!-- Resets grid for mobile -->
+	<link rel="stylesheet" href="css/tempS.css" media="handheld, only screen and (max-width: 500px)" />
+	
+	<!-- FONTELLO CODES -->
+	<link rel="stylesheet" href="css/fontello.css">
+    <link rel="stylesheet" href="css/animation.css"><!--[if IE 7]>
+    <link rel="stylesheet" href="css/fontello-ie7.css"><![endif]-->
+    <script>
+      function toggleCodes(on) {
+        var obj = document.getElementById('icons');
+        
+        if (on) {
+          obj.className += ' codesOn';
+        } else {
+          obj.className = obj.className.replace(' codesOn', '');
+        }
+      }
+      
+    </script>
+	
+	
+	
+	
+	<!-- GOOGLE ANALYICS BEGIN -->
+	<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-12577956-2']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+	<!-- GOOGLE ANALYICS END -->
+		
+	
+	
+	
+ <script type="text/javascript" src="http://use.typekit.com/abs2lma.js"></script>
+<script type="text/javascript"> try{Typekit.load();}catch(e){}</script>
+ </head>
+
+
+
+ 
+ <body>
+
+ <div id="ptn">
+ <div id="bgcontain">
+ <div id="contain">
+
+<h1 class="">Hi there. I'm Chris
+
+<!-- <span class="waves">Don't like the background color? Click <a href="index.php">here</a> to try again</span> -->
+</h1> 
+<div class="fade-in two">
+<p>I design impactful experiences across screens and paper. I'm currently designing for a multi-screen world at <a href="http://www.findawayworld.com">Findaway.</a> </p> 
+
+
+
+<div class="social" class="">
+<!-- <div class="twitter">
+
+
+
+
+
+
+<div id="twitterBTN"><a href="http://twitter.com/cseeds" class="twitter-follow-button" data-button="grey" data-text-color="333333" link-color="333333" data-width="130px" data-show-count="false">Follow @cseeds</a>
+<script src="http://platform.twitter.com/widgets.js" type="text/javascript"></script></div>
+
+</div>-->
+
+
+<div class="rightcol">
+
+
+
+<div class="links" >
+
+<a href="mailto:hello@christopherseeds.com?subject=Hi Chris, lets talk about" class="icon-mail"></a>
+<a href="https://www.twitter.com/cseeds"  class="icon-twitter" ></a> 
+<a href="https://www.facebook.com/cseeds" class="icon-facebook-squared"></a> 
+<a href="http://www.linkedin.com/in/cseeds" class="icon-linkedin"></a> 
+<a href="http://www.rdio.com/people/cseeds/" class="icon-rdio"></a> 
+
+<!-- <a href="http://instagram.com/cseeds class="icon-instagram"></a> --> 
+
+<!-- <a href="http://svpply.com/cseeds" title="Svpply" id="sv">Svpply</a> -->
+
+
+
+<!-- <hr> -->
+<!-- 
+<div id="slider" style="width:100%; background-color:gray;">
+  <ul>
+  	<li style="display:block">one</li>
+  	<li style="display:none">two</li>
+  	<li style="display:none">three</li>
+  	<li style="display:none">four</li>
+  	<li style="display:none">five</li>
+  </ul>
+</div>
+ -->
+
+<!-- text based social nav -->
+<!-- <b>Links</b> ::  <a href="http://forrst.me/cseeds/">Forrst</a> | <a href="http://www.last.fm/user/cseeds" title="LastFM"> LastFM</a> | <a href="http://svpply.com/cseeds" title="Svpply">Svpply</a>  | <a href="http://www.linkedin.com/in/cseeds" title="LinkedIn">LinkedIn</a> | <a href="https://www.facebook.com/cseeds" title="Facebook">Facebook</a> -->
+
+</div>
+<br>
+
+
+
+<!-- <div class="question"><p><a href="http://imgs.christopherseeds.com/">Inspiring</a> &amp; <a href="http://fun.christopherseeds.com/">Entertaining</a> </p></div>-->
+
+
+
+
+<!-- end rightcol div --></div>
+
+
+<!-- end social div --></div>
+
+
+<!--
+
+<div class="trick">
+
+<div class="desktop">It looks like you are <b>on a desktop</b>.</div>
+
+<div class="tablet">It looks like you are <b>on a tablet or small netbook</b>.</div>
+
+<div class="mobile">It looks like you are <b>on a smartphone</b>.
+
+
+
+</div>
+-->
+
+</div>
+</div><!-- end fade -->
+
+
+</div> <!-- end contain  -->
+</div> <!-- end BGcontain  -->
+ <!-- end ptn  -->
+ 
+
+ 
+
+ 
+ </body>
+ <script src='swipe.js'></script>
+  <script>
+new Swipe(document.getElementById('slider'));
+
+</script>
+</html>
